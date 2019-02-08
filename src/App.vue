2959c063cd76300aa94dbb8bca7546608e102d8f
Pedro Mendonca <peumendonca@gmail.com>
2019-02-08 19:10:21 +0000

    Add basics with GitHub integration

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Try GitHub Actions</h1>
+    <p>
+      This project is a demonstration of a CI/CD pipeline with GitHub Actions. 
+      Hosted on <a href="https://heroku.com">Heroku</a>
+    </p>
+    <p>If you would like to get more information about this project make sure to check out the links below</p>
+    <a href="https://github.com/pedsm/tryGithubAction">GitHub repo</a>
+    <!--  TODO: Updated this    -->
+    <br/>
+    <a href="#">Blog Post(coming soon)</a>
+
+    <h2>Contributors</h2>
+    <div id="userContainer"> 
+      <user v-for="user in users" :key="user" :username="user"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from './components/User.vue'
+import users from './users.json'
 
 export default {
-  name: 'app',
+  name: 'tryGithubAction',
   components: {
-    HelloWorld
-  }
+    User
+  },
+  data: () => ({
+    users
+  })
 }
 </script>
 
@@ -24,5 +41,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a {
+  color: #58CCED;
+}
+
+a:hover {
+  color: #1261AD;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <img v-if="userInfo != null" :src="this.userInfo.avatar_url" width="100"/>
+    <p>{{username}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: {
+    username: String
+  },
+  data: () => ({
+    userInfo: null
+  }),
+  created() {
+      fetch(`https://api.github.com/users/${this.username}`)
+        .then(res => res.json())
+        .then(json => this.userInfo = json)
+        .catch(e => console.error(e))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+img {
+  border-radius: 100px;
+}
+</style>
